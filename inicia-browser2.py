00cfe73934c3ed0f9e892d8a3d71a02979f4791a
pedro.pacheco.a@gmail.com <pedro.pacheco.a@gmail.com>
2018-05-30 10:04:28 -0300

    Consulta no google

--- inicia-browser2.py
+++ inicia-browser2.py
@@ -1,23 +1,26 @@
 from selenium import webdriver
 
+
 class Google:
-    def __init__(self,driver):
+    def __init__(self, driver):
         self.driver = driver
         self.url = 'http://google.com.br'
-        self.search_bar = 'lst-ib'
-        self.btn_search = 'btnK'
+        self.search_bar = 'lst-ib' # id
+        self.btn_search = 'btnK' # Name
+        self.btn_lucky = 'btnI' # name
 
     def navigate(self):
         self.driver.get(self.url)
 
     def search(self, word='None'):
         self.driver.find_element_by_id(self.search_bar).send_keys(word)
+        self.driver.find_element_by_name(self.btn_search).click()
 
 
 ff = webdriver.Firefox()
-
-
 g = Google(ff)
+g.navigate()
+g.search('Pedro Alexandr Pacheco')
 
 #g.navigate()
 
